eb8db2a7d2248139a257143039ae2bbd8e5aae02
MostakemHossain <mostakememon123@gmail.com>
2023-12-31 10:53:20 +0600

    student get all student,single student,createstudents

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import cors from "cors";
 import express from "express";
+import { StudentRoutes } from "./app/models/student/student.route";
 const app = express()
 
 
 //parsers
 app.use(express.json())
 app.use(cors());
+
+app.use('/api/v1/students',StudentRoutes);
+
 app.get('/', (req, res) => {
    
   res.send('Hello World!')
---
+++ src/app/models/student/student.service.ts
@@ -0,0 +1,21 @@
+import { TStudent } from "./student.interface";
+import { Student } from "./student.model";
+
+const createStudentIntoDB=async(payload:TStudent)=>{
+    const result= await Student.create(payload);
+    return result;
+}
+const getAllStudentFromDB= async()=>{
+    const result= await Student.find();
+    return result;
+}
+const getSingleStudentFromDB= async(id:string)=>{
+    const result =await Student.findById(id);
+    return result;
+}
+
+export const StudentServices= {
+    createStudentIntoDB,
+    getAllStudentFromDB,
+    getSingleStudentFromDB,
+}
